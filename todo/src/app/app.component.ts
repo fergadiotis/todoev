48743d612563ba56d7acb44c2b0a3e44f2516bc7
fergadiotis <fergadiotis@gmail.com>
2024-10-24 17:38:31 -0400

    todo Complete

---
+++ todo/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { TodoList } from "./todoList";
+import { TodoItem } from "./todoItem";
+import { FormsModule } from '@angular/forms'
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule,
+    MatButtonModule, MatToolbarModule, MatIconModule, MatBadgeModule,
+    MatTableModule, MatCheckboxModule, MatFormFieldModule, MatInputModule,
+    MatSlideToggleModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  private list = new TodoList("Bob", [
+    new TodoItem("Go for run", true),
+    new TodoItem("Get flowers"),
+    new TodoItem("Collect tickets"),
+   ]);
+   get username(): string {
+    return this.list.user;
+    }
+   get itemCount(): number {
+    return this.list.items
+      .filter(item => !item.complete).length;
+     }
+    get items(): readonly TodoItem[] {
+      return this.list.items.filter(item => this.showComplete || !item.complete);
+     }
+
+    addItem(newItem: string) {
+     if (newItem != "") {
+     this.list.addItem(newItem);
+     }
+     }
+
+  showComplete: boolean = false;
+}
